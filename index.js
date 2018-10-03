96eb50f6d0ea29791d9759f53b78bd158fc6188c
Henry Brooke-Dean <henrybrookedean@hotmail.com>
2018-10-03 14:03:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions and code here
+function destructivelyAppendKitten() {
+kittens.push();
+return kittens;
+}
+
+function destructivelyPrependKitten() {
+  kittens.unshift();
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArray = kittens.slice();
+  newArray.push(name)
+  return newArray
+}
